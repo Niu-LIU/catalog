088321b6bc2d778c9b7fafd770dfccc28d7c530f
liuniu@smail.nju.edu.com <Neo@SUZAKU-2.local>
2019-03-03 10:53:12 +0800

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name: test.py
+"""
+Created on Sun Mar  3 10:53:06 2019
+
+@author: Neo(liuniu@smail.nju.edu.cn)
+"""
+
+import numpy as np
+
+
+# -----------------------------  FUNCTIONS -----------------------------
+
+
+# --------------------------------- END --------------------------------
